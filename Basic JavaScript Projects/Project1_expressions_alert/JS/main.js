3d0870f655ec1451f82daeaae12d58dd6907cd77
Bradley Robles <bradleyrobles88@gmail.com>
2022-08-29 14:20:33 -0500

    Added //Submission assignment requirements

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -11,3 +11,9 @@ document.write(C); //text on site that write variable C
 var Pets="Dogs", Dog1=" Cali", Dog2=" Sally"; //Variable with multiple other varibles and values
 document.write(Dog2); //Text on site that writes variable Dog2
 document.write(A+B+C+Dog1); //Text on site that writes variables A, B, C and, Dog1
+
+//Submission assignment requirments
+var Sent1 = " This is the beginning of the string"; //Varible Sent1 that equals string value "This is the beginning of the string"
+var Sent2 = " and this is the end of the string" //Varible Sent2 that equals string value "and this is the end of the string"
+document.write (Sent1 + Sent2) //Statements that writes concatenated values Sent1 and Sent2
+window.alert (Sent1 + Sent2) //Window alert that displays the concatenated values Sent1 and Sent2
