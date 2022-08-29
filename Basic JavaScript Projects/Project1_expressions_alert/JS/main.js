07f72200aa0e169dacd4b27a3ae08af12865f471
Bradley Robles <bradleyrobles88@gmail.com>
2022-08-29 14:08:40 -0500

    Added JavaScript Project Folder.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+window.alert ("Hello, world!"); //alert window saying "Hello, world!"
+document.write("Hello, world!"); //Text on site saying "Hello, world!"
+var A = " This is a string"; //variable that equals " This is a string"
+document.write(A);  // Text on site that write variable A
+var B = "Your awesome!"; //variable that equals " Your awesome"
+window.alert(B); // Text on site that write variable A
+document.write("<br> I said to the World \"Hello, World!\" and the World said," // Text on site that is concatenated
++ " \"Yo!\" ");
+var C = "Concatenated" + " String"; // Variable that equals to concatenated words
+document.write(C); //text on site that write variable C
+var Pets="Dogs", Dog1=" Cali", Dog2=" Sally"; //Variable with multiple other varibles and values
+document.write(Dog2); //Text on site that writes variable Dog2
+document.write(A+B+C+Dog1); //Text on site that writes variables A, B, C and, Dog1
